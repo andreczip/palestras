e40b0ec6f682f7b6f29b089c0456714e5a2f1adc
andreczip <andreczip@gmail.com>
2013-09-23 11:33:06 -0300

    Add service

--- angularjs/examples/list/js/TodoCtrl.js
+++ angularjs/examples/list/js/TodoCtrl.js
@@ -1,9 +1,14 @@
-function TodoCtrl($scope) {
+
+function TodoCtrl($scope, TodoService) {
+
   $scope.todos = [
     {text:'learn angular', done:true},
     {text:'build an angular app', done:false}];
 
+  $scope.list = TodoService.list();
+
   $scope.addTodo = function() {
+    console.log($scope.list)
     $scope.todos.push({text:$scope.todoText, done:false});
     $scope.todoText = '';
   };
